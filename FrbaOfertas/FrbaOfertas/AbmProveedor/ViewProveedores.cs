943e400bb7336c7b98a5c8cff4651dff1fc11071
sebastianUgartemendia1 <ugartemendia.sebastian1@gmail.com>
2019-10-24 21:03:50 -0300

    Agrego view de Provedores y pantalla Alta de Proveedor

---
+++ FrbaOfertas/FrbaOfertas/AbmProveedor/ViewProveedores.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaOfertas.AbmProveedor
+{
+    public partial class ViewProveedores : Form
+    {
+        public ViewProveedores()
+        {
+            InitializeComponent();
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnBuscarTodos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxRubro_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRazonSocial_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
